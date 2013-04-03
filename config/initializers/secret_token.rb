8379102ba1e2319a498884e8c66a654021f58e2a
fender707 <fender707@rambler.ru>
2013-04-03 10:54:04 +0300

    Int_shop - Start page, with some products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '613b08ec018f1656e4c101483c0fa152f5bd925669ac653d623bbda832d7e75eab74ea0fa21fae60888d1a882a8b504094411069aaa05aac0df554b36a383ef9'
